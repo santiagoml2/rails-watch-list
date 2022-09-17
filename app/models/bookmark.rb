60efeb002884d13e2db64e4766fb0ef7413b564c
santiago munoz <santiagoml2@hotmail.com>
2022-09-17 14:23:24 -0500

    test approved

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  belongs_to :movie
+  belongs_to :list
+  validates_presence_of :movie, :list
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates_uniqueness_of :movie_id, { scope: :list_id }
+end
