597a1675668f6f537ff98e6db613717fd0fb59e9
santiago munoz <santiagoml2@hotmail.com>
2022-09-17 17:09:22 -0500

    Frontend in progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
+
+  resources :lists do
+    resources :bookmarks
+  end
+
+  resources :movies
 end
---
+++ db/migrate/20220917213958_add_image_to_lists.rb
@@ -0,0 +1,4 @@
+class AddImageToLists < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
